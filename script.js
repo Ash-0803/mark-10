a198e3e7a2cbc5f78fa0796e9c1fd95004212259
Ashish Chaudhary <ashish080303@gmail.com>
2022-12-09 09:05:07 +0530

    all functionalities added

--- script.js
+++ script.js
@@ -2,18 +2,38 @@ const cashGiven = document.querySelector('#cash-given');
 const billAmt = document.querySelector('#bill-amount');
 const btn = document.querySelector('#calculate');
 const cell = document.querySelectorAll('.no-of-notes');
+const message = document.querySelector('#message');
 
 const notes = [2000,500,100,20,10,5,1];
 
-function clickHandle(){
+btn.addEventListener('click', ()=>{
+  const given = cashGiven.value;
+  const amt = billAmt.value;
+  hideMessage();
+  if(amt>0){
+    if(given >= amt){
+      let rtrn = given-amt;
+      clickHandle(rtrn)
+    } 
+    else{
+      showMessage("Would you mind joining us washing plates?")
+    }
+  } 
+  else{
+    showMessage("Invalid Bill Amount");
+  }
+});
+function clickHandle(rtrn){
   notes.forEach((aNote,index)=>{
-    const given = cashGiven.value;
-    const amt = billAmt.value;
-    let rtrn = given-amt;
     let noOfNotes = Math.floor(rtrn/aNote);
     rtrn%=aNote;
     cell[index].innerText = noOfNotes; 
   })
 }
-
-btn.addEventListener('click', clickHandle);
+function showMessage(mssg){
+  message.innerText = mssg
+  message.style.visibility = "visible";
+}
+function hideMessage(){
+  message.style.visibility = "hidden";
+}
